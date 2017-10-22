67b4874cd885c16ac87636fae8028baf98c8c72b
sbukharov <sergey.bukharov@cluberudite.com>
2017-10-22 15:12:06 -0700

    Added roles and role switching to site, displaying role on maintenance page.

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -4,23 +4,24 @@ class Mtce extends Application {
 
         public function index()
         {
-                $this->data['pagetitle'] = 'TODO List Maintenance';
-                $tasks = $this->tasks->all(); // get all the tasks
+            $role = $this->session->userdata('userrole');
+            $this->data['pagetitle'] = 'TODO List Maintenance ('. $role . ')';
+            $tasks = $this->tasks->all(); // get all the tasks
 
-                // substitute the status name
-                foreach ($tasks as $task)
-                        if (!empty($task->status))
-                                                $task->status = $this->app->status($task->status);
+            // substitute the status name
+            foreach ($tasks as $task)
+                    if (!empty($task->status))
+                                            $task->status = $this->app->status($task->status);
 
-                // build the task presentation output
-                $result = '';   // start with an empty array        
-                foreach ($tasks as $task)
-                        $result .= $this->parser->parse('oneitem',(array)$task,true);
+            // build the task presentation output
+            $result = '';   // start with an empty array        
+            foreach ($tasks as $task)
+                    $result .= $this->parser->parse('oneitem',(array)$task,true);
 
-                // and then pass them on
-                $this->data['display_tasks'] = $result;
-                $this->data['pagebody'] = 'itemlist';
-                $this->render();
+            // and then pass them on
+            $this->data['display_tasks'] = $result;
+            $this->data['pagebody'] = 'itemlist';
+            $this->render();
         }
 
 }
